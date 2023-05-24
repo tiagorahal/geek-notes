0f019ce839efab86a5a36970bd106b4bd192dece
tiagorahal <rahal.aires@gmail.com>
2023-05-24 13:16:06 -0300

    Create basic posts structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+20.times do
+  Post.create(
+    title: Faker::Fantasy::Tolkien.race,
+    author_user: Faker::Fantasy::Tolkien.character,
+    description: Faker::Fantasy::Tolkien.poem,
+    image: Faker::Placeholdit.image(size: '300x300', format: 'jpg', background_color: :random, text_color: :random, text: Faker::Movies::Hobbit.thorins_company),
+    likes: rand(0..15)
+  )
+end
